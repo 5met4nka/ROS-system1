e5171ba75a9502e1664b972506490a273f844614
Nikita Loabzev <pavilich99@mail.ru>
2022-11-07 08:13:52 +0300

    f

--- src/scripts/request_service.py
+++ src/scripts/request_service.py
@@ -6,24 +6,24 @@ import rospy # импортируем основной модуль `rospy`
 from system1.srv import poly, polyResponse # импортируем модуль типа сервиса `Poly` вместе с типом ответа
 
 def handle_request_service(req):
-    sumFromRequestService = req.x1 + req.x2 ** 2
-    rospy.loginfo("---")
-    rospy.loginfo("processing request...")
-    rospy.loginfo("returning [%s + %s^2 = %s]" % (req.x1, req.x2, sumFromRequestService))
-    rospy.loginfo("---")
+	sumFromRequestService = req.x1 + req.x2 ** 2
+	rospy.loginfo("---")
+	rospy.loginfo("processing request...")
+	rospy.loginfo("returning [%s + %s^2 = %s]" % (req.x1, req.x2, sumFromRequestService))
+	rospy.loginfo("---")
 
-    resp = polyResponse()
-    resp.sumFromRequestService = sumFromRequestService
+resp = polyResponse()
+resp.sumFromRequestService = sumFromRequestService
 
-    return resp
+return resp
 
 def request_service():
-    rospy.init_node('request_service') # инициализация узла
-    s = rospy.Service('request_service', poly, handle_request_service) # передаем имя сервиса,
-    # тип сообщений и функцию обработчика
-    rospy.loginfo("request service ready to calc.") # вывод в терминал информации
-    rospy.spin() # будет удерживать программу рабочей до тех пор,
-    # пока ROS не завершится или узел не бует прерван
+	rospy.init_node('request_service') # инициализация узла
+	s = rospy.Service('request_service', poly, handle_request_service) # передаем имя сервиса,
+	# тип сообщений и функцию обработчика
+	rospy.loginfo("request service ready to calc.") # вывод в терминал информации
+	rospy.spin() # будет удерживать программу рабочей до тех пор,
+	# пока ROS не завершится или узел не бует прерван
 
 
 request_service() # запускаем этот сервис
